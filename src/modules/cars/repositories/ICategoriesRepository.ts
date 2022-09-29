07ac09c711e1a68b3d347033d7723aafd724a8bd
kaikySantos <info.kaiky@gmail.com>
2022-09-29 14:39:49 -0300

    feat: category use case

--- src/modules/cars/repositories/ICategoriesRepository.ts
+++ src/modules/cars/repositories/ICategoriesRepository.ts
@@ -1,9 +1,14 @@
 import { Category } from "../model/Category";
 
-interface ICategoryRepository {
-  findByName(name: string): Category;
-  list(): Category;
-  create(name: string, description: string): void;
+interface ICreateCategory {
+  name: string;
+  description: string;
 }
 
-export { ICategoryRepository };
+interface ICategoriesRepository {
+  findByName(name: string): Category;
+  list(): Category;
+  create({ name, description }: ICreateCategory): void;
+}
+
+export { ICategoriesRepository };
--- src/modules/cars/services/CreateSpecificationService.ts
+++ src/modules/cars/services/CreateSpecificationService.ts
@@ -1,6 +1,25 @@
+import { ISpectificationsRepository } from "../repositories/ISpecificationRepository";
+
+interface IRequest {
+  name: string;
+  description: string;
+}
+
 class CreateSpecificationService {
-  execute() {
-    console.log("Hello!");
+  constructor(private specificationRepository: ISpectificationsRepository) {}
+
+  execute({ name, description }: IRequest) {
+    const specificationAlreadyExists =
+      this.specificationRepository.findByName(name);
+
+    if (specificationAlreadyExists) {
+      throw new Error("Specification already exists!");
+    }
+
+    this.specificationRepository.create({
+      name,
+      description,
+    });
   }
 }
 
