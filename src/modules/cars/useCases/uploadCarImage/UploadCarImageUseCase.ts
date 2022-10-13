2dfe7fe1501cd4bccdec8c31f72d7d3d1d672dba
kaikySantos <info.kaiky@gmail.com>
2022-10-13 19:15:52 -0300

    feat: upload car image use case

--- src/modules/cars/useCases/uploadCarImage/UploadCarImageUseCase.ts
+++ src/modules/cars/useCases/uploadCarImage/UploadCarImageUseCase.ts
@@ -1,1 +1,24 @@
-class UploadCArImageUseCase {}
+import { inject, injectable } from "tsyringe";
+
+import { ICarsImageRepository } from "@modules/cars/repositories/ICarsImageRepository";
+
+interface IRequest {
+  car_id: string;
+  images_name: string[];
+}
+
+@injectable()
+class UploadCArImageUseCase {
+  constructor(
+    @inject("CarsImagesRepository")
+    private carsImagesRepository: ICarsImageRepository
+  ) {}
+
+  async execute({ car_id, images_name }: IRequest): Promise<void> {
+    images_name.map(async (image) => {
+      await this.carsImagesRepository.create(car_id, image);
+    });
+  }
+}
+
+export { UploadCArImageUseCase };
