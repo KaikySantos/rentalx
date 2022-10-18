9034850a1a862de600877c5ea3c54504a89e80c3
kaikySantos <info.kaiky@gmail.com>
2022-10-18 20:39:47 -0300

    refactor: refresh token

---
+++ src/config/auth.ts
@@ -0,0 +1,7 @@
+export default {
+  secret_token: "614cd0aeb44d4152eef631d838e34fb4",
+  expires_in_token: "15m",
+  secret_refresh_token: "e16a279daa7f2bff5f4d6f0fbe655407",
+  expires_in_refresh_token: "30d",
+  expires_refresh_token_days: 30,
+};
